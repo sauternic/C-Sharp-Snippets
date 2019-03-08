323da8330c12f3423ab9a38299729494e29fb3bc
Nicolas Sauter <sauternic@users.noreply>
2019-03-08 01:09:50 +0100

    WPF IValueConverter

---
+++ WPF-IValueConverter.cs
@@ -0,0 +1,43 @@
+In App.xaml
+-----------
+
+      <Application x:Class="WpfApp3.App"
+                 xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+                 xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+                 xmlns:local="clr-namespace:WpfApp3"
+                 StartupUri="MainWindow.xaml">
+            <Application.Resources>
+                <local:MeinConverter x:Key="MeinConverter" />
+            </Application.Resources>        -------------
+      </Application>
+
+*******************************************************************************************************************************************************************
+
+In MainWindow.xaml
+------------------
+
+    <StackPanel>
+        <CheckBox Margin="0 100" Name="checkbox1" HorizontalAlignment="Center" IsChecked="True" VerticalAlignment="Center"/>
+        <Button Margin="10" Content="Button" Width="50" IsEnabled="{Binding ElementName=checkbox1, Path=IsChecked, Converter={StaticResource MeinConverter}}"/>                                                                                                                                     -------------
+    </StackPanel>                                                                                                                            -------------
+    
+*******************************************************************************************************************************************************************   
+    
+In MainWindow.xaml.cs
+---------------------
+    
+    public class MeinConverter : IValueConverter
+    {                            ---------------
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Hier wird nur der boolean umgekehrt und zurückgeschickt! ;)
+			return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+**********************************************************************************************************************************************************************
