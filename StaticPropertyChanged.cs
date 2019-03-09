dc0d927dfc123c438c1f96981c750842ed427114
Nicolas Sauter <sauternic@users.noreply>
2019-03-10 00:57:43 +0100

    xaml ergänzt

--- StaticPropertyChanged.cs
+++ StaticPropertyChanged.cs
@@ -1,3 +1,5 @@
+**************************************MainWindow.xaml.cs************************************************************
+
 using System.ComponentModel;
 using System.Windows;
 using System;
@@ -61,3 +63,21 @@ namespace WpfApp2
     }
 
 }
+
+********************************MainWindow.xaml**************************************************************************
+
+<Window x:Class="WpfApp2.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:WpfApp2"
+        mc:Ignorable="d"
+        Title="MainWindow" Height="450" Width="800">
+    <Grid Background="Aquamarine">
+        <TextBlock Name="textblock1" Text="{Binding Path=(local:CMeine.Name)}"  Background="White" HorizontalAlignment="Left" Height="22" Margin="166,120,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="387"/>
+        <Button Content="Property Ändern" HorizontalAlignment="Left" Height="46" Margin="261,255,0,0" VerticalAlignment="Top" Width="208" Click="Button_Click"/>
+
+    </Grid>
+ </Window>
+
